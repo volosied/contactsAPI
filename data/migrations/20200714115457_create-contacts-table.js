c045aeb04435356c49e5bf2d51e338894d993f46
DaBusyB <brandiapetsi@gmail.com>
2020-07-14 13:31:43 -0400

    Create db with knex migrations

--- data/migrations/20200714115457_create-contacts-table.js
+++ data/migrations/20200714115457_create-contacts-table.js
@@ -1,8 +1,13 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema.createTable('contacts', tbl => {
+      tbl.increments();
+      tbl.text('name', 128).notNullable();
+      tbl.integer('number').unique().notNullable();
+      tbl.text('email').unique()
+  })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists('contacts')
 };
